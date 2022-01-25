1086b7a05a98492c8ef45995bfea5fc9125e6ca2
chryslimjy <75465315+chryslimjy@users.noreply.github.com>
2022-01-25 12:00:40 +0800

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,38 @@
+/**
+ * hello
+ */
+public class Lab1 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, I am Chrystal");
+
+
+    String module = "1009";
+    switch (module) {
+      case "1010":
+        System.out.println("Computer Networks");
+        break;
+      case "1006":
+        System.out.println("Mathematics 2");
+        break;
+      case "1009":
+        System.out.println("Object Oriented Programming");
+        break;
+      case "2902":
+        System.out.println("Career and Professional Development");
+        break;
+      case "1007":
+        System.out.println("Operating Systems");
+        break;
+    }
+    
+
+    for (int x = 102; x >= 66; x--) {
+      if (x % 2 == 1) {
+        System.out.println("Value of x : " + x);
+      }
+
+    }
+
+  }
+}
